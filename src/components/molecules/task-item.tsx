c89ed1df7fcc122cfd11804d412a3818678c75f1
Tri Nguyen <shyssthanhtri@gmail.com>
2024-10-11 21:47:28 +0700

    feat: implement task item

--- src/components/molecules/task-item.tsx
+++ src/components/molecules/task-item.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import React, { useMemo, useState } from "react";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import { type ITaskDto } from "@/dtos/task/task.dto";
+import { cn } from "@/lib/utils";
 
 interface TaskItemProps {
   task: ITaskDto;
 }
 export const TaskItem = ({ task }: TaskItemProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const expandedButton = useMemo(() => {
+    if (!task.description) return null;
+    const Icon = isExpanded ? ChevronUp : ChevronDown;
+    return (
+      <Button
+        variant="ghost"
+        size="sm"
+        className="ml-2 h-auto p-1"
+        onClick={() => setIsExpanded(!isExpanded)}
+        aria-label={isExpanded ? "Hide description" : "Show description"}
+      >
+        <Icon className="h-3 w-3" />
+      </Button>
+    );
+  }, [isExpanded, task.description]);
+
   return (
     <Card>
-      <CardContent className="pt-6">{task.title}</CardContent>
+      <CardContent className="pt-6">
+        <div className="flex items-center">
+          <Checkbox className="mr-4" />
+          {task.title}
+          {expandedButton}
+        </div>
+        <div
+          className={cn(
+            `grid pb-0 transition-all duration-300 ease-in-out`,
+            isExpanded
+              ? "grid-rows-[1fr] opacity-100"
+              : "grid-rows-[0fr] opacity-0",
+          )}
+        >
+          <div className="overflow-hidden">
+            <p className="mt-4 text-gray-600">{task.description}</p>
+          </div>
+        </div>
+      </CardContent>
     </Card>
   );
 };
